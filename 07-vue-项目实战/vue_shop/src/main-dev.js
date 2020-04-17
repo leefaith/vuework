701a1d01e7ed19c909df0b322921d7740935fcfd
leefengjiao <981815087@qq.com>
2020-04-17 10:44:38 +0800

    优化开发

--- 07-vue-项目实战/vue_shop/src/main-dev.js
+++ 07-vue-项目实战/vue_shop/src/main-dev.js
@@ -14,15 +14,29 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+//导入NProgress对应js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
 import axios from 'axios'
+import nProgress from 'nprogress'
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+    //在request拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
-    // console.log(config)
-    config.headers.Authorization = window.sessionStorage.getItem('token')
-        // 在最后必须 return config
+        // console.log(config)
+        nProgress.start()
+        config.headers.Authorization = window.sessionStorage.getItem('token')
+            // 在最后必须 return config
+        return config
+    })
+    //在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+    nProgress.done()
     return config
 })
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
