f0d8cd94b00907a315ddf237137f7ae745ff0eb9
fionalee <981815087@qq.com>
2020-04-27 14:29:37 +0800

    cloudeMusic

---
+++ 09-es6和cloudmusic/06_单文件组件.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h2 @click="sayHEllo">标题</h2>
+  </div>
+</template>
+
+<script>
+export default {
+methods: {
+  sayHello(){
+    alert('hello')
+  }
+},
+}
+</script>
+
+<style>
+.app{
+  background-color: skyblue;
+}
+</style>
