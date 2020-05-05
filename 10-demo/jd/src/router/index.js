5a7810962f36f48e005049cbb4c7a28e1ad5eead
fionalee <981815087@qq.com>
2020-05-05 14:49:41 +0800

    jd

---
+++ 10-demo/jd/src/router/index.js
@@ -0,0 +1,70 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
+Vue.use(Router)
+
+export default new Router({
+  //mode: 'history',
+ // base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      redirect: '/login'
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import( '../views/Login.vue')
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: () => import('../views/Index.vue')
+    },
+    {
+      path: '/botnav',
+      name: 'botnav',
+      component: () => import('../views/Botnav.vue'),
+      children:[
+        {
+          path: 'index',
+          name: 'index',
+          component: () => import('../views/Index.vue')
+        },
+        {
+          path: 'list',
+          name: 'list',
+          component: () => import('../views/List.vue')
+        },
+        {
+          path: 'search',
+          name: 'search',
+          component: () => import('../views/Search.vue')
+        },
+        {
+          path: 'cart',
+          name: 'cart',
+          meta:{
+            requireAuth:true,//当有这个字段的时候，我们就认为他这个路由页面是要有登录权限的
+          },
+          component: () => import('../views/Cart.vue')
+        },
+        {
+          path: 'mine',
+          name: 'mine',
+          meta:{
+            requireAuth:true,//当有这个字段的时候，我们就认为他这个路由页面是要有登录权限的
+          },
+          component: () => import('../views/Mine.vue')
+        },
+      ]
+    }
+  ]
+})
